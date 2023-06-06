b9d9ed55c8b1b8f6c4ed636ec3dff2dd51aa5e46
muhamad.mursaleen <mmursaleen@ap.org>
2023-06-06 17:37:33 +0500

    resolve issue in TwoSum problem

--- twoSum.py
+++ twoSum.py
@@ -1,25 +1,30 @@
 from typing import List
 
+nums = [1, 2, 3, 4]
+
+for inner_val in range(len(nums)):
+    print(inner_val)
+
 
 def twoSum(nums: List[int], target: int) -> List[int]:
     initialTwoSum = nums[0] + nums[1]
-    next_index = 0
-
     targeted_arr = []
-
     if initialTwoSum == target:
         return [0, 1]
 
     else:
-        for val in range(1, len(nums) - 1):
-            next_index = val + 1
-            if nums[val] + nums[next_index] == target:
-                targeted_arr.append(val)
-                targeted_arr.append(next_index)
+        for val in range(len(nums)):
+            if len(targeted_arr) == 0:
+                for inner_val in range(val + 1, len(nums)):
+                    if nums[val] + nums[inner_val] == target:
+                        targeted_arr.append(val)
+                        targeted_arr.append(inner_val)
+                        break
 
     return targeted_arr
 
-arr = [3,2,4]
-target = 6
+
+arr = [2, 5, 5, 11]
+target = 10
 
 twoSum(arr, target)
